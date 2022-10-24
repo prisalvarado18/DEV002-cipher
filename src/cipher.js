33c8851e3264acf20c8b05aaaf5b01ac634299a1
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2022-10-24 17:24:55 -0500

    Nuevos cambios
    Realicé nuevos cambios que incluyen el código del cifrado :)

--- src/cipher.js
+++ src/cipher.js
@@ -3,20 +3,21 @@ const cipher = {
   //Crear el metodo cipher.encode(offset,string) para CIFRAR
   encode:(offset, string)=>{
     //Convertir el offset ingresado en numero y almacenarlo en una variable
-    var posicion = parseInt(offset);
+    const posicion = parseInt(offset);
     //Crear una variable que almacene el input del usuario
-    var mensaje ="";
+    let mensajeCifrado ="";
     //Crear un ciclo que recorra cada un de las letras del input del usuario
     //y que sume las posiciones indicadas
-    var i = 0;
+    let i = 0;
     for (i;i<string.length;i++){
-      var antes = string.charCodeAt(i);
-      var despues = (antes-65+posicion)%26+65;
+      let antes = string.charCodeAt(i);
+      let despues = (antes-65+posicion)%26+65;
       mensajeCifrado+=String.fromCharCode(despues);
     }
     return mensajeCifrado;
   }
   //Crear el metodo cipher.decode(offset,string)
+  
 };
 
-export default cipher;
+//export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
+//Llamar al input del usuario
+let llamarCadena = document.getElementById("mensaje");
+//Llamar al numero de posiciones que ingresa el usuario
+let llamarPosicion = document.getElementById("posicion");
+//Mostrar el resultado
+let respuesta = document.getElementById("respuesta");
+let resultado = document.getElementById("resultado");
+let mensajeFinal = document.getElementById("mensajeFinal");
 
-console.log(cipher);
+const codificar = () => {
+    let string = llamarCadena.value;
+    let offset = llamarPosicion.value;
+    
+    respuesta.style.display = "block";
+    mensajeFinal.innerHTML = "Mensaje codificado: ";
+    resultado.innerHTML = cipher.encode(offset,string);
+};
+
+//console.log(cipher);
