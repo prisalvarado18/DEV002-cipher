e6fdad47f995eb1216eced0b30b11fac94d83d9d
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2022-10-31 02:50:03 -0500

    Realicé cambios en index.js
    Añadí cambios que había hecho aparte y además creé la variable para mostra el texto descfrado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 //import cipher from './cipher.js';
-//Llamar al input del usuario
-let llamarCadena = document.getElementById("mensaje");
-//Llamar al numero de posiciones que ingresa el usuario
-let llamarPosicion = document.getElementById("posicion");
-//Mostrar el resultado
-let respuesta = document.getElementById("respuesta");
-let resultado = document.getElementById("resultado");
-let mensajeFinal = document.getElementById("mensajeFinal");
 
-const codificar = () => {
-    let string = llamarCadena.value;
-    let offset = llamarPosicion.value;
-    
-    respuesta.style.display = "block";
-    mensajeFinal.innerHTML = "Mensaje codificado: ";
-    resultado.innerHTML = cipher.encode(offset,string);
-};
+let ingreso_texto = document.getElementById("ingreso_texto");
+let ingreso_desplazamientos = document.getElementById("ingreso_desplazamientos");
+let output = document.getElementById("output");
+
+
+const mostrarMensajeCifrado =()=>{
+    let cadena = ingreso_texto.value;//Guarda al texto ingresado por el usuario inicialmente
+    let desplazamiento = ingreso_desplazamientos.value;//Guarda a la clave ingresada por el usuario inicialmente
+    output.innerHTML = cipher.encode(desplazamiento,cadena);//Cambia el texto con el resultado del método 
+}
+
+const mostrarMensajeDescifrado =()=>{
+    let cadena = ingreso_texto.value;//Guarda al texto ingresado por el usuario inicialmente
+    let desplazamiento = ingreso_desplazamientos.value;//Guarda a la clave ingresada por el usuario inicialmente
+    output.innerHTML = cipher.decode(desplazamiento,cadena);//Cambia el texto con el resultado del método 
+}
 
 //console.log(cipher);
+//console.log(codificar);
