2dc69a78b2384c231feec6813286fcee488565e3
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2022-11-03 05:32:08 -0500

    Agregué controles
    Controles para mayúsculas, minúsculas y caracteres especiales. Los espacios y el resto de caracteres se imprimen tal cual

--- src/cipher.js
+++ src/cipher.js
@@ -24,12 +24,35 @@ function encode (offset,string) {
   for(contador;contador<longitudCadena;contador++){ 
     //Crear una variable que almacene cada caracter del texto ingresado por el usuario
     //y devuelva un número indicando su valor Unicode
-    let antes = string.charCodeAt(contador);
-    //Emplear la fórmula
-    let despues = (antes - 65 + numeroDesplazamientos) % 26 + 65;
-    //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
-    //el cual usará la una secuencia de valores Unicode
-    textoCifrado += String.fromCharCode(despues);
+    let unicodeIngresado = string.charCodeAt(contador);
+    //Crear un controlador para minúsculas
+    if(unicodeIngresado > 96 && unicodeIngresado < 123){
+      //Emplear la fórmula
+      let unicodeCifrado = (unicodeIngresado - 97 + numeroDesplazamientos) % 26 + 97;
+      //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
+      //el cual usará la una secuencia de valores Unicode
+      textoCifrado += String.fromCharCode(unicodeCifrado);
+    }
+    //Crear un controlador para mayúsculas
+    else if(unicodeIngresado > 64 && unicodeIngresado < 91){
+      //Emplear la fórmula
+      let unicodeCifrado = (unicodeIngresado - 65 + numeroDesplazamientos) % 26 + 65;
+      //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
+      //el cual usará la una secuencia de valores Unicode
+      textoCifrado += String.fromCharCode(unicodeCifrado);
+    }
+    //Cifrar números y caracteres especiales
+    else if(unicodeIngresado > 32 && unicodeIngresado < 65){
+      let unicodeCifrado = (unicodeIngresado - 32 + numeroDesplazamientos) % 32 + 32;
+      textoCifrado += String.fromCharCode(unicodeCifrado);
+    }
+    else if(unicodeIngresado > 122 && unicodeIngresado < 255){
+      let unicodeCifrado = (unicodeIngresado - 123 + numeroDesplazamientos) % 132 + 123;
+      textoCifrado += String.fromCharCode(unicodeCifrado);
+    //Imprimir espacios y el resto de caracteres tal cual
+    }else{
+      textoCifrado += String.fromCharCode(unicodeIngresado);
+    }
   }
   return textoCifrado;
 }
@@ -53,12 +76,35 @@ function decode (offset,string)  {
   for(contador;contador<longitudCadena;contador++){ 
     //Crear una variable que almacene cada caracter del texto ingresado por el usuario
     //y devuelva un número indicando su valor Unicode
-    let antes = string.charCodeAt(contador);
-    //Emplear la fórmula
-    let despues = (antes - 90 - numeroDesplazamientos) % 26 + 90;
-    //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
-    //el cual usará la una secuencia de valores Unicode
-    textoDescifrado += String.fromCharCode(despues);
+    let unicodeIngresado = string.charCodeAt(contador);
+    //Crear un controlador para minúsculas
+    if(unicodeIngresado > 96 && unicodeIngresado < 123){
+      //Emplear la fórmula
+      let unicodeDescifrado = (unicodeIngresado - 122 - numeroDesplazamientos) % 26 + 122;
+      //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
+      //el cual usará la una secuencia de valores Unicode
+      textoDescifrado += String.fromCharCode(unicodeDescifrado);
+    }
+    //Crear un controlador para mayúsculas
+    else if(unicodeIngresado > 64 && unicodeIngresado < 91){
+      //Emplear la fórmula
+      let unicodeDescifrado = (unicodeIngresado - 90 - numeroDesplazamientos) % 26 + 90;
+      //Almacenar en la variable textoCifrado la cadena mediante el uso del método String.fromCharCode()
+      //el cual usará la una secuencia de valores Unicode
+      textoDescifrado += String.fromCharCode(unicodeDescifrado);
+    }
+    //Descifrar números y caracteres especiales
+    else if(unicodeIngresado > 32 && unicodeIngresado < 65){
+      let unicodeDescifrado = (unicodeIngresado - 64 - numeroDesplazamientos) % 32 + 64;
+      textoDescifrado += String.fromCharCode(unicodeDescifrado);
+    }
+    else if(unicodeIngresado > 122 && unicodeIngresado < 255){
+      let unicodeDescifrado = (unicodeIngresado - 254 - numeroDesplazamientos) % 132 + 254;
+      textoDescifrado += String.fromCharCode(unicodeDescifrado);
+    //Imprimir espacios y el resto de caracteres tal cual
+    }else{
+      textoDescifrado += String.fromCharCode(unicodeIngresado);
+    }
   }
   return textoDescifrado;
 }
